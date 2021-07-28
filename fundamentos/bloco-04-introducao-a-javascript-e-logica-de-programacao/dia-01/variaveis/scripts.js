73c47f99da58bbf83db6a947974de48fa7f8e63c
Antônio Campos <aclcampos@live.com>
2021-07-28 16:00:54 -0300

    Conteúdos - Variáveis

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01/variaveis/scripts.js
@@ -0,0 +1,10 @@
+const myName = 'Antonio'; 
+const birthCity = 'Cabo_Sto_Agostinho';
+let birthYear = 1991;
+
+birthYear = 2030
+birthCity = 'Outro'
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
