372921ae2ea63458f2ad47c3a0c05a072f3d47bd
Antônio Campos <aclcampos@live.com>
2021-08-03 11:28:55 -0300

    Exercícios 4.3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03/scripts-exercicio3.js
@@ -0,0 +1,11 @@
+let n = 5
+
+if (n > 1) {
+    for (let linha = 1 ; linha <= n ; linha += 1) {
+        let ast = ''
+        for (let espaco = 1; espaco <= n - 1; espaco += 1 ) 
+            ast += '1'
+            console.log(ast)
+        }
+        
+    }
