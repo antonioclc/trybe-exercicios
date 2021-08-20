edcf892d870d0f19d51ca040eb68214ac0e85dad
Antônio Campos <aclcampos@live.com>
2021-08-19 21:15:44 -0300

    Iniciando exercícios do bloco 06

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/script.js
@@ -0,0 +1,15 @@
+const statesBR = ['AC','AL','AP','AM','BA','CE','ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF']
+
+function addStates () {
+    for (let i = 0; i < statesBR.length; i += 1) {
+        const addOption = document.createElement('option')
+        const states = document.getElementById('states')
+        states.appendChild(addOption)
+        states[i].innerText = statesBR[i]
+        states[i].value = statesBR[i]
+        states[i].id = statesBR[i]
+        console.log(states[i])
+    }
+}
+
+addStates();
